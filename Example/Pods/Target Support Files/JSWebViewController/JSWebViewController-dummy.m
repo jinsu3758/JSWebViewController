3d97ab2a10a453a9d18d877f4d057ac811d49264
박진수 <jinsu@bagjinsuui-iMac.local>
2018-10-23 11:37:08 +0900

    git init

---
+++ Example/Pods/Target Support Files/JSWebViewController/JSWebViewController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_JSWebViewController : NSObject
+@end
+@implementation PodsDummy_JSWebViewController
+@end
---
+++ JSWebViewController/Classes/JSWebViewController.swift
@@ -0,0 +1,29 @@
+//
+//  JSWebViewController.swift
+//  JSWebViewController
+//
+//  Created by 박진수 on 2018. 10. 23..
+//
+
+import UIKit
+
+class JSWebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
